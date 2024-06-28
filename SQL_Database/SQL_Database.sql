13b063ae19fa04c1a1909eb60069908796400abf
Silver1695 <thang16092005@gmail.com>
2024-06-28 21:35:21 +0700

    Thang_test

--- SQL_Database/SQL_Database.sql
+++ SQL_Database/SQL_Database.sql
@@ -26,3 +26,21 @@ MaLT nvarchar,
 TenLT nvarchar,
 GhiChu nvarchar
 );
+
+-- SQL Thang
+CREATE TABLE NhaPhanPhoi (
+	MaNPP NVARCHAR(50) PRIMARY KEY,
+    Ten NVARCHAR(50) NOT NULL,
+    DiaChi NVARCHAR(50) NOT NULL,
+    Email NVARCHAR(50) NOT NULL,
+    SDT NVARCHAR(10) NOT NULL
+);
+
+CREATE TABLE HoaDon (
+    MaHD NVARCHAR(50) PRIMARY KEY,
+    SoLuong INT NOT NULL,
+    ThanhTien FLOAT NOT NULL,
+    NgayThang DATETIME NOT NULL,
+    MaNV NVARCHAR(50) NOT NULL,
+    MaKH NVARCHAR(50) NOT NULL
+);
